d726a36c5343a25ea5cb9b0ea840d89b67ea3fea
Mac_fall <fall545@github.com>
2025-06-18 18:18:01 +0800

    tokenizer complete

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # cumtber
 parser
+说明:
+getNextToken函数执行后会把token写入CurTok
+getNextToken是会消耗token的，如果想要跳过 token也可以调用函数来实现
+
+要求:
+
+所有的.cpp放到src目录下,.h放到include目录中
+例如
+project_root/
+├── CMakeLists.txt
+├── main.cpp
+├── src/
+│   ├── foo.cpp
+│   ├── bar.cpp
+├── include/
+│   ├── foo.h
+│   ├── bar.h
+
+文件命名规范
+写parser的时候要单独新建一个.cpp的文件,如要完成expression的parser那就新建expressionast.cpp
+不同的parser要放到不同的.cpp中,并把对外开放的接口函数原型写到同名的.h中如expressionast.h
+
+函数的本体要写到.cpp中,声明必须统一放到同名.h文件中,方便调用
+
+变量命名不做规范了
+
+
+注意
+若添加了新的文件一定要重新编译
+cmake ..
+make
+
+TODO:
+恺哥 完成函数
+振宇 expression
+宋 完成循环 
+
+编译方法:
+cd 项目所在根目录
+mkdir build
+cd build
+cmake ..
+make
---
+++ include/tokenizer.h
@@ -0,0 +1,88 @@
+#include <algorithm>
+#include <cassert>
+#include <cctype>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <map>
+#include <memory>
+#include <string>
+#include <vector>
+
+static std::string IdentifierStr; // Filled in if tok_identifier
+static double NumVal;             // Filled in if tok_number
+enum{
+  tok_eof = -1,//这个是结束的标志,不在你们的语法解析里,不用管
+
+  tok_def = -2,//def
+  tok_extern = -3,
+
+  tok_identifier = -4,
+  tok_number = -5
+};
+static int gettok() {
+  static int LastChar = ' ';
+
+  // Skip any whitespace.
+  while (isspace(LastChar))
+    LastChar = getchar();
+
+  if (isalpha(LastChar)) { // identifier: [a-zA-Z][a-zA-Z0-9]*
+    IdentifierStr = LastChar;
+    while (isalnum((LastChar = getchar())))
+      IdentifierStr += LastChar;
+
+    if (IdentifierStr == "def")
+      return tok_def;
+    if (IdentifierStr == "extern")
+      return tok_extern;
+    return tok_identifier;
+  }
+
+  if (isdigit(LastChar) || LastChar == '.') { // Number: [0-9.]+
+    std::string NumStr;
+    do {
+      NumStr += LastChar;
+      LastChar = getchar();
+    } while (isdigit(LastChar) || LastChar == '.');
+
+    NumVal = strtod(NumStr.c_str(), nullptr);
+    return tok_number;
+  }
+
+  if (LastChar == '#') {
+    // Comment until end of line.
+    do
+      LastChar = getchar();
+    while (LastChar != EOF && LastChar != '\n' && LastChar != '\r');
+
+    if (LastChar != EOF)
+      return gettok();
+  }
+
+  // Check for end of file.  Don't eat the EOF.
+  if (LastChar == EOF)
+    return tok_eof;
+
+  // Otherwise, just return the character as its ascii value.
+  int ThisChar = LastChar;
+  LastChar = getchar();
+  return ThisChar;
+}
+
+static int CurTok;
+static int getNextToken() { return CurTok = gettok(); }
+
+
+static std::map<char, int> BinopPrecedence;//   +-*/ = 10 10 20 20 
+
+static int GetTokPrecedence() {
+  if (!isascii(CurTok))
+    return -1;
+
+  // Make sure it's a declared binop.
+  int TokPrec = BinopPrecedence[CurTok];
+  if (TokPrec <= 0)
+    return -1;
+  return TokPrec;
+}
